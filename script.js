4b5c05b1f38303258b64412bfa3002749361b608
janos03 <janosmikaczo@gmail.com>
2020-11-20 08:27:57 +0100

    Finomítások
    Js kijavítva, css- hez hozzádva, plusz görgetési animáció, hogy ne legyen annyira szolid az oldal

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function check() {
     var d = Number(document.getElementById('szobeli').value);
     var osszesen = document.getElementById('eredmeny').innerHTML = b + a + c + d;
     var pont = 0;
-
+    emeltpont = 0;
 
     if (osszesen >= 120) {
         pont = 5;
@@ -19,12 +19,28 @@ function check() {
         pont = 1;
     }
 
-
-
-    if (szobeli >= 3, 75) {
-        pont = 1
+    if (osszesen >= 90) {
+        emeltpont = 5;
+    } else if (osszesen >= 70.5) {
+        emeltpont = 4;
+    } else if (osszesen >= 49.5) {
+        emeltpont = 3;
+    } else if (osszesen >= 37.5) {
+        emeltpont = 2;
+    } else {
+        emeltpont = 1;
     }
-    var ja = Number(document.getElementById('mennyi').innerHTML = pont);
+
+
+
+
+    if (szobeli <= 3.75 || a + b + c <= 38) {
+        pont = 1
+        emeltpont = 1;
+    }
+    var ja = Number(document.getElementById('mennyi').innerHTML = "középszinten:" + " " +
+        pont);
+    var jaa = Number(document.getElementById('emeltmennyi').innerHTML = "emeltszinten:" + " " + emeltpont);
 
     return false;
 
